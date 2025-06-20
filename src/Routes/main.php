b938bed8e5918277f00d9c7d7194928770035d48
Thainã Viadroski <thainav2010@gmail.com>
2025-06-20 14:05:04 -0300

    Add: gitignore

--- src/Routes/main.php
+++ src/Routes/main.php
@@ -8,3 +8,4 @@ Routes::post('/users/login', 'UserController@login');
 Routes::get('/users/fetch', 'UserController@fetch');
 Routes::update('/users/update', 'UserController@update');
 Routes::delete('/users/{id}/delete', 'UserController@delete');
+
